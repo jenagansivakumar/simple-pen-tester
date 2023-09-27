dedc638e4b353409deff2b4d161261627906d05d
Jena <jenagansivakumar@gmail.com>
2023-09-27 21:57:42 +0200

    backend is confirmed to work

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,11 +10,22 @@ app.use(cors());
 app.get("/api/check", async (req, res) => {
   const targetUrl = req.query.url as string;
 
+  // Simple validation: Check if URL is not empty and has HTTP/HTTPS prefix.
+  if (!targetUrl || !targetUrl.startsWith("http")) {
+    return res.status(400).json({ status: "Invalid URL provided" });
+  }
+
   try {
     const response = await fetch(targetUrl, { method: "HEAD" });
     res.json({ status: response.status.toString() });
   } catch (error) {
-    res.json({ status: "Error fetching" });
+    console.error("Error while fetching the target URL:", error); // Log the error for server-side clarity
+
+    if (error instanceof Error) {
+      res.status(500).json({ status: `Fetch error: ${error.message}` }); // Send the error message to the client.
+    } else {
+      res.status(500).json({ status: "Unknown fetch error" }); // Send a generic error message for non-Error types
+    }
   }
 });
 
--- src/Components/UrlChecker.tsx
+++ src/Components/UrlChecker.tsx
@@ -4,29 +4,45 @@ export default function UrlChecker() {
   const [url, setUrl] = useState<string>("");
   const [status, setStatus] = useState<string | null>(null);
 
-  const checkUrl = async (): Promise<void> => {
+  const checkUrl = async (
+    event: React.MouseEvent<HTMLButtonElement>
+  ): Promise<void> => {
+    event.preventDefault(); // prevent the default form submission
+
     try {
-      const response = await fetch("/api/check?url=" + url);
+      const response = await fetch(
+        `http://localhost:4000/api/check?url=${url}`
+      );
+
+      if (!response.ok) {
+        throw new Error(`Network response was not ok: ${response.statusText}`);
+      }
+
       const data = await response.json();
       setStatus(data.status);
     } catch (error) {
-      setStatus("Error fetching");
+      console.error("There was a problem with the fetch operation:", error);
+
+      const errorMessage =
+        error instanceof Error ? error.message : "An unexpected error occurred";
+      setStatus(`Error: ${errorMessage}`);
     }
   };
+
   return (
     <div className="url-checker">
-      <h1> URL Checker</h1>
+      <h1>URL Checker</h1>
       <form>
-        <label htmlFor="url"> URL </label>
+        <label htmlFor="url">URL</label>
         <input
-          type="string"
+          type="text"
           name="url"
           value={url}
           onChange={(event) => setUrl(event.target.value)}
           placeholder="Enter URL"
         />
-        <button onClick={checkUrl}> Check URL</button>
-        <h2> Status: {status} </h2>
+        <button onClick={checkUrl}>Check URL</button>
+        <h2>Status: {status}</h2>
       </form>
     </div>
   );
